211040a35ba48fc6ce09d7973b259efba87774c5
Sadialiou Diallo <seriadiallo1@gmail.com>
2024-08-27 17:38:57 +0000

    Personalisation d'un modelform

--- articles/forms.py
+++ articles/forms.py
@@ -11,6 +11,15 @@ from .models import Article
 
 class ArticleForm(forms.ModelForm):
 
+    # tag = forms.CharField(max_length=240, min_length=2, required=True,
+    #                       widget=forms.TextInput(attrs={'class': 'form-control'}))
+
     class Meta:
         model = Article
         fields = ('title', 'sumary', 'date_pub', 'content')
+        widgets = {
+            "content": forms.Textarea(attrs={"cols": 80, "rows": 20}),
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'sumary': forms.TextInput(attrs={'class': 'form-control'}),
+            'date_pub': forms.TextInput(attrs={'class': 'form-control', 'type': 'date'}),
+        }
