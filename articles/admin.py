00203cd21afb6b7054ac4c636d60eeb9aced639b
Sadialiou Diallo <seriadiallo1@gmail.com>
2024-12-14 04:48:26 +0000

    integration du django admin pour les articles

--- articles/admin.py
+++ articles/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Article
+# from .forms import ArticleForm
 
-# Register your models here.
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'sumary', 'date_pub', 'user', 'created_at')
+    date_hierarchy = "date_pub"
+    # form = ArticleForm
+    # fields = (('title', 'sumary'), ('date_pub', 'user'), 'content', 'cover')
+    fieldsets = [
+        (
+            "BAse",
+            {
+                "fields": ["title", "sumary", "date_pub", ],
+            },
+        ),
+        (
+            "Description",
+            {
+                "classes": ["collapse"],
+                "fields": ["content", "cover"],
+            },
+        ),
+    ]
+
+
+admin.site.register(Article, ArticleAdmin)
